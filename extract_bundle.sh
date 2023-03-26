7c72944332deb48d8469d47175cd575718662370
Rozcietrzewiacz <rozcietrzewiacz@gmail.com>
2023-03-27 00:11:31 +0200

    operator: a bunch of improvements
    Most noteworthy:
    - `lib.jq`:
      * Add rbacGlobal function, which generates ServiceAccount, ClusterRole
        and ClusterRoleBining based on the input/output specification within
        our CustomResource
      * Add deploy functions to easily promote a pod to a deployment
    - `jq-controller-operator.jq`:
      * Actually use all of the `JqController` CustomResource fields
      * Chown all objects generated by the controller to the above CR
      * tidy up the mess with projected volume used to pass downwardAPI
        fields as jq variables - and actually propagate labels in a sensible fashion
    
    Also improved the bundle extraction method: use a bundle.list.jsonl file
    as base to `k get -f` all the bundled resources, instead of hardcoding
    the list within extract_bundle.sh

--- extract_bundle.sh
+++ extract_bundle.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
-kubectl create ns jq-ctr --dry-run=client -o yaml > jq-controller-bundle.yaml 
-kubectl get \
-	deployments.apps/jq-controller-operator \
-	cm/jq-controller \
-	cm/jq-controller-operator.jq \
-	cm/lib.jq \
-	cm/templates \
-	sa/jq-adm \
-	clusterrole/cluster-admin-jq \
-	clusterrolebinding/cluster-admin-jq \
-	crd/jqcontrollers.xxx.jq \
-	-o yaml \
+kubectl get -f bundle.list.jsonl -o yaml \
 | yq -P '.items[]
-	|.metadata |=
+	| .metadata |=
 		( del(.annotations)
 		| del(.uid)
 		| del(.creationTimestamp)
 		| del(.resourceVersion)
 		| del(.generation)
 		)
-	|del(.status)' \
+	| del(.status)
+	| del(.secrets)' \
 | sed 's/^apiVersion/---\n\0/' \
->> jq-controller-bundle.yaml
+> jq-controller-bundle.yaml
 
 wc -l jq-controller-bundle.yaml
